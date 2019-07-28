18a92a67247aa9358f353538bddca55beca7e85e
Dani Roxberry <dani@bitoriented.com>
2019-07-27 18:33:00 -0700

    [fix] major cleanup before coin endpoint release

--- api/app.py
+++ api/app.py
@@ -1,33 +1,70 @@
+import os
+import exceptions
+import metadata
+import sentry_sdk
+
 from flask import Flask, jsonify
-from metadata import get_color_attributes
+from sentry_sdk.integrations.flask import FlaskIntegration
+
+from dotenv import load_dotenv
+load_dotenv()
 
 
+sentry_sdk.init(dsn=os.getenv('SENTRY_DSN'), integrations=[FlaskIntegration()])
 app = Flask(__name__)
 
 
-@app.route('/api/coin/<token_id>')
-def rainbowcoin(token_id):
-    info = get_color_attributes(token_id)
+@app.route('/api/coin/<rgb_id>')
+def rainbowcoin(rgb_id):
+    """Returns JSON-serialized ERC-721 metadata for a single RainbowCoin."""
 
+    # Validate that rgb_id is a valid RGB integer.
+    if int(rgb_id) not in range(0, 16777216):
+        raise exceptions.InvalidUsage(
+            f'{rgb_id} is not a valid RGB integer. '
+            'Valid RGB integers range between 0 and 16,777,215.',
+            status_code=500)
+
+    # Get all the data for this RainbowCoin.
+    info = metadata.get_color_info(rgb_id)
+
+    # Remove internal-only attributes before returning public metadata.
+    remove = ['red', 'green', 'blue', 'title', 'description', 'image']
+    external_attrs = info.copy()
+    for key in remove:
+        del(external_attrs[key])
+    attrs = metadata.get_color_attributes(external_attrs)
+
+    # Render OpenSea-compatible JSON metadata.
+    # See https://docs.opensea.io/docs/2-adding-metadata for more.
     return jsonify({
-        'name': info['name'],
-        'description': f"A {info['name'].lower()} colored RainbowCoin.",
+        'background_color': 'FFFFFF',
+        'description': info['description'],
+        'external_url': f"https://rainbowco.in/coin/{rgb_id}",
+        'name': info['title'],
         'image': info['image'],
-        'external_url': f"https://rainbowco.in/coin/{token_id}",
-        'attributes': info
+        'attributes': attrs,
     })
 
 
-@app.route('/api/factory/<token_id>')
-def factory(token_id):
-    token_id = int(token_id)
-    return jsonify({
-        'name': "One RainbowCoin",
-        'description': "When you purchase this option, you will receive one RainbowCoin!",
-        'image': 'https://storage.googleapis.com/rainbowco.in/factory/random-coin.png',
-        'external_url': 'https://rainbowco.in/factory/%s' % token_id,
-        'attributes': []
-    })
+@app.errorhandler(exceptions.InvalidUsage)
+def handle_invalid_usage(error):
+    """Handles exceptions and returns a helpful error to the user."""
+    response = jsonify({'error': error.to_dict().get('message')})
+    response.status_code = error.status_code
+    return response
+
+
+# @app.route('/api/factory/<rgb_id>')
+# def factory(rgb_id):
+#     rgb_id = int(rgb_id)
+#     return jsonify({
+#         'name': "One RainbowCoin",
+#         'description': "When you purchase this option, you will receive one RainbowCoin!",
+#         'image': 'https://storage.googleapis.com/rainbowco.in/factory/random-coin.png',
+#         'external_url': 'https://rainbowco.in/factory/%s' % rgb_id,
+#         'attributes': []
+#     })
 
 
 if __name__ == '__main__':
--- api/metadata.py
+++ api/metadata.py
@@ -1,3 +1,5 @@
+import webcolors
+import string
 
 from colourlovers import clapi
 from google.cloud import storage
@@ -13,31 +15,67 @@ COIN_PADDING = 3
 COIN_SIZE = 500 - COIN_PADDING
 
 
-def get_color_attributes(token_id):
-    """Return everything we know about this token_id (an RGB integer)."""
-    hex_code = _get_hex(token_id)
+def get_color_info(rgb_id):
+    """Return everything we know about this rgb_id (an RGB integer)."""
+    hex_code = _get_hex(rgb_id)
+    hex_hash = title = f"#{hex_code}"
+
+    # Call ColourLovers API and search for the hex color.
     cl = clapi.ColourLovers()
     clr = cl.search_color(hexvalue=hex_code, format='json')[0]
+
+    # Gather color stats and attributes.
+    rgb_percent = webcolors.hex_to_rgb_percent(hex_hash)
     lum = _get_luminance(clr.RGB.red, clr.RGB.blue, clr.RGB.green)
-    url = _compose_image(token_id, clr.RGB.red,
+
+    # Normalize color title. Default is the hex code (with hash prefix).
+    title_is_hex = clr.title.upper() == hex_code
+    if not title_is_hex:
+        # We have a named color. Remove all punctuation from the string.
+        title = clr.title.translate(str.maketrans(
+            '', '', string.punctuation)).title()
+
+    # Generate image assets and upload them to Google Storage Cloud.
+    url = _compose_image(rgb_id, clr.RGB.red,
                          clr.RGB.green, clr.RGB.blue, lum)
+
     return {
-        'name': clr.title.title(),
-        'hex': clr.hex,
+        'title': title,
+        'description': f'A {title if title_is_hex else title.lower()} colored RainbowCoin.',
+        'rgb_integer': int(rgb_id),
+        'hex_code': hex_hash,
+        'percentage_of_red': float(rgb_percent.red.replace('%', '')),
+        'percentage_of_green': float(rgb_percent.green.replace('%', '')),
+        'percentage_of_blue': float(rgb_percent.blue.replace('%', '')),
+        'hsv': f'({clr.HSV.hue}, {clr.HSV.saturation}, {clr.HSV.value})',
+        'rgb': f'({clr.RGB.red}, {clr.RGB.green}, {clr.RGB.blue})',
+        'luminance': float("%.2f" % lum),
+        'image': url,
         'red': clr.RGB.red,
         'green': clr.RGB.green,
         'blue': clr.RGB.blue,
-        'hue': clr.HSV.hue,
-        'saturation': clr.HSV.saturation,
-        'value': clr.HSV.value,
-        'rank': clr.rank,
-        'luminance': lum,
-        'image': url
     }
 
 
-def _compose_image(token_id, red, green, blue, lum, path="coins"):
-    """Create a RainbowCoin, saved to images/output/{token_id}.png"""
+def get_color_attributes(info_dict):
+    """Convert color information into OpenSea-compatible attributes."""
+    attrs = []
+    for key, value in info_dict.items():
+        attr = {'trait_type': key, 'value': value, 'display_type': 'string'}
+        if key == 'luminance':
+            attr['display_type'] = 'boost_number'
+        else:
+            if isinstance(value, int) or isinstance(value, float):
+                attr['display_type'] = 'number'
+            if key.startswith('percentage_of'):
+                attr['display_type'] = 'boost_percent'
+
+        attrs.append(attr)
+    return attrs
+
+
+def _compose_image(rgb_id, red, green, blue, lum, path="coins"):
+    """Create a RainbowCoin, saved to images/output/{rgb_id}.png"""
     bkg = Image.new('RGBA', (COIN_SIZE + COIN_PADDING,
                              COIN_SIZE + COIN_PADDING), (0, 0, 0, 0))
 
@@ -47,11 +85,11 @@ def _compose_image(token_id, red, green, blue, lum, path="coins"):
 
     base_img = COIN_BLACK if lum > 30.0 else COIN_WHITE
     base = Image.open(base_img).convert("RGBA")
-    output_path = "images/output/%s.png" % token_id
+    output_path = "images/output/%s.png" % rgb_id
     composite = Image.alpha_composite(bkg, base)
     composite.save(output_path)
 
-    blob = _get_bucket().blob(f"{path}/{token_id}.png")
+    blob = _get_bucket().blob(f"{path}/{rgb_id}.png")
     blob.upload_from_filename(filename=output_path)
     return blob.public_url
 
@@ -68,9 +106,9 @@ def _get_bucket():
     return client.get_bucket(GOOGLE_STORAGE_BUCKET)
 
 
-def _get_hex(token_id):
-    """Convert token_id (an RGB integer) to RGB, then return the corresponding hex code."""
-    tmp, b = divmod(int(token_id), 256)
+def _get_hex(rgb_id):
+    """Convert rgb_id (an RGB integer) to RGB, then return the corresponding hex code."""
+    tmp, b = divmod(int(rgb_id), 256)
     tmp, g = divmod(tmp, 256)
     alpha, r = divmod(tmp, 256)
     return "{:02x}{:02x}{:02x}".format(r, g, b).upper()
