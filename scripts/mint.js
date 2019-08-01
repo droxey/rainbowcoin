6b4899c9f4ffcb3b210a63f86f8415fb88515092
Dani Roxberry <dani@bitoriented.com>
2019-07-31 21:59:55 -0700

    [fix] improve mint script

--- scripts/mint.js
+++ scripts/mint.js
@@ -1,6 +1,7 @@
 const HDWalletProvider = require("truffle-hdwallet-provider");
 const web3 = require('web3');
 
+
 const MNEMONIC = process.env.MNEMONIC;
 const INFURA_KEY = process.env.INFURA_KEY;
 const NFT_CONTRACT_ADDRESS = process.env.NFT_CONTRACT_ADDRESS;
@@ -12,6 +13,8 @@ if (!MNEMONIC || !INFURA_KEY || !OWNER_ADDRESS || !NETWORK) {
     return
 }
 
+const START_COIN = 2342;
+const COINS_TO_MINT = 1;
 const NFT_ABI = [{
     "constant": false,
     "inputs": [
@@ -31,26 +34,22 @@ const NFT_ABI = [{
     "type": "function"
 }];
 
-
 async function main() {
     const provider = new HDWalletProvider(MNEMONIC, `https://${NETWORK}.infura.io/v3/${INFURA_KEY}`);
     const web3Instance = new web3(provider);
 
-    // Black, Red, OrangeYellow, Green, Blue, Purple, White
-    const coinsToMint = [0, 15874875, 16765264, 5234049, 3582698, 11444714, 16777215];
-
     if (NFT_CONTRACT_ADDRESS) {
         const nftContract = new web3Instance.eth.Contract(NFT_ABI, NFT_CONTRACT_ADDRESS, { gasLimit: "1000000" });
 
-        for (var i = 0; i < coinsToMint.length; i++) {
-          const result = await nftContract.methods.mintTo(OWNER_ADDRESS, coinsToMint[i]).send({
+        for (var tokenId = START_COIN; tokenId < (START_COIN + COINS_TO_MINT); tokenId++) {
+          const result = await nftContract.methods.mintTo(OWNER_ADDRESS, tokenId).send({
             from: OWNER_ADDRESS
           });
 
-          console.log("Minted RainbowCoin #" + coinsToMint[i] + "! Transaction:", result.transactionHash);
+          console.log("[" + result.transactionHash + "]", "Minted RainbowCoin #" + tokenId + " at", new Date().toTimeString());
         }
 
-        console.log("Minting complete!", coinsToMint.length, "coins minted.");
+        console.log("Minting complete!", COINS_TO_MINT, "coins minted.");
     }
 }
 
